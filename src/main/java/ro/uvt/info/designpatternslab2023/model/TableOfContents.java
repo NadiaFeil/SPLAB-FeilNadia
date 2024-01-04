711f061c6b9ddc12b45ea15d58ad5d86ebc16e6d
OWNER <nadia.feil02@e-uvt.ro>
2024-01-04 14:28:58 +0200

    change the implement of TableOfContent

--- src/main/java/ro/uvt/info/designpatternslab2023/model/TableOfContents.java
+++ src/main/java/ro/uvt/info/designpatternslab2023/model/TableOfContents.java
@@ -1,39 +1,48 @@
 package ro.uvt.info.designpatternslab2023.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import ro.uvt.info.designpatternslab2023.dao.Element;
 import ro.uvt.info.designpatternslab2023.dao.Visitee;
 import ro.uvt.info.designpatternslab2023.dao.Visitor;
 
-public class TabelOfContents implements Element, Visitee {
-    private String text;
+public class TableOfContents implements Element, Visitee {
+    private List<String> entries;
 
-    public TabelOfContents(String text) {
-        this.text = text;
+    public TableOfContents() {
+        this.entries = new ArrayList<>();
+    }
+
+    public void addEntry(String entry) {
+        entries.add(entry);
     }
 
     @Override
     public void print() {
-        System.out.println(text);
+        System.out.println("Table of Contents:");
+        for (String entry : entries) {
+            System.out.println(entry);
+        }
     }
 
     @Override
     public void addElement(Element element) {
-
+        // Not applicable for TableOfContents
     }
 
     @Override
     public void removeElement(Element element) {
-
+        // Not applicable for TableOfContents
     }
 
     @Override
     public Element get(int number) {
+        // Not applicable for TableOfContents
         return null;
     }
 
     @Override
     public void accept(Visitor visitor) {
-
         visitor.visitTabelOfContents(this);
     }
 }
